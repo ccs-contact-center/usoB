46f6b5cc0605895f828057cb505c9c1a66af7a33
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-07-29 10:03:13 -0500

    Update DespedidaView.js

--- src/views/temarioViews/DespedidaView.js
+++ src/views/temarioViews/DespedidaView.js
@@ -17,16 +17,12 @@ class PresentacionView extends Component {
         <CardBody className="">
           <Row>
             <Col xs="12" sm="12" md="12" className="centrado-fila mt-3">
-       
-                
                 <ReactPlayer
                   className="animated bounceInRight delay-0s"
                   width="65%"
                   url={usoB}
                   controls
-             
                 />
-           
             </Col>
           </Row>
         </CardBody>
